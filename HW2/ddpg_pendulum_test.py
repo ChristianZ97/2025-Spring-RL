4d778effb6131a1acc53c88f1d92d7fa7bedc9a9
Christian Jeng <kristan970724@gmail.com>
2025-04-22 01:46:04 +0800

    Test for Pendulum

--- HW2/ddpg_pendulum_test.py
+++ HW2/ddpg_pendulum_test.py
@@ -166,8 +166,7 @@ class Critic(nn.Module):
         x = self.fc1(inputs)
         x = F.relu(x)
 
-        x = torch.cat([x, actions], 1)
-        x = self.fc2(x)
+        x = self.fc2(torch.cat([x, actions], 1))
         x = F.relu(x)
 
         x = self.fc3(x)

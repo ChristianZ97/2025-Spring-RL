e3d28c55c864e813ede6e6d77908831897e0b74e
Christian Jeng <kristan970724@gmail.com>
2025-04-23 11:33:03 +0800

    Re-optimize

--- HW2/ddpg_cheetah.py
+++ HW2/ddpg_cheetah.py
@@ -285,7 +285,8 @@ class DDPG(object):
             eval_scaled_action = self.actor.forward(inputs=state_batch)
 
             self.critic.eval()
-            policy_q = self.critic.forward(inputs=state_batch, actions=eval_scaled_action).detach()
+            with torch.no_grad():
+                policy_q = self.critic.forward(inputs=state_batch, actions=eval_scaled_action)
             policy_loss = -policy_q.mean()
             # policy_loss = -self.critic.forward(inputs=state_batch, actions=eval_scaled_action).mean()
 

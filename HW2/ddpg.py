72afe50292cdc34cca1ac66b841bffc6010dc4bb
Christian Jeng <kristan970724@gmail.com>
2025-04-25 01:22:38 +0800

    Network tuning

--- HW2/ddpg.py
+++ HW2/ddpg.py
@@ -370,7 +370,7 @@ def train(
 
             state_tensor = torch.tensor(state_np, dtype=torch.float32)
             
-            if total_numsteps < 5000:
+            if total_numsteps < 2500:
                 action_np = env.action_space.sample()
             else:
                 with torch.no_grad():
@@ -390,7 +390,7 @@ def train(
             if done_np: break
         # End of one interacted episode
 
-        if len(memory) >= 5000:
+        if len(memory) >= 2500:
             for _ in range(updates_per_step):
 
                 batch = memory.sample(batch_size)

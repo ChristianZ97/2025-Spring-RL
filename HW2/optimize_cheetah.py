7c7b163ac4b704a1f137c41f3e71f2b348f84f3c
Christian Jeng <kristan970724@gmail.com>
2025-04-24 20:15:34 +0800

    Tuning network for cheetah

--- HW2/optimize_cheetah.py
+++ HW2/optimize_cheetah.py
@@ -186,5 +186,5 @@ def run_optimization(n_calls=20, n_random_starts=5, output_dir='optimization_res
 
 if __name__ == '__main__':
     # Run optimization with 30 total evaluations, 10 random
-    result, final_model = run_optimization(n_calls=150, n_random_starts=50)
+    result, final_model = run_optimization(n_calls=50, n_random_starts=20)
     print("Optimization and visualization completed!")

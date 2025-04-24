177e4a4f6855bd569b181c1aa050dea8abb5a0b1
ChristianZ97 <124138449+ChristianZ97@users.noreply.github.com>
2025-04-24 12:41:16 +0800

    Update ddpg.py

--- HW2/ddpg.py
+++ HW2/ddpg.py
@@ -372,7 +372,7 @@ def train(
 
             state_tensor = torch.tensor(state_np, dtype=torch.float32)
             
-            if total_numsteps < 2000:
+            if total_numsteps < 1000:
                 action_np = env.action_space.sample()
             else:
                 with torch.no_grad():
@@ -392,7 +392,7 @@ def train(
             if done_np: break
         # End of one interacted episode
 
-        if len(memory) >= 2000:
+        if len(memory) >= 1000:
             for _ in range(updates_per_step):
 
                 batch = memory.sample(batch_size)

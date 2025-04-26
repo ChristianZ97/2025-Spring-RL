462d811b3a0a37ed1dc56f2e22bdfeeedd41c215
Christian Jeng <kristan970724@gmail.com>
2025-04-27 00:19:26 +0800

    Pendulum - rewards scale to 10% w/ Critic L2 Reg 1e-2 -> 1e-3

--- HW2/New Try/Pendulum/algo.py
+++ HW2/New Try/Pendulum/algo.py
@@ -39,8 +39,8 @@ class DDPG(object):
 
         self.critic = Critic(hidden_size, self.num_inputs, self.action_space).to(self.device)
         self.critic_target = Critic(hidden_size, self.num_inputs, self.action_space).to(self.device)
-        self.critic_optim = Adam(self.critic.parameters(), lr=lr_c)
-        # self.critic_optim = Adam(self.critic.parameters(), lr=lr_c, weight_decay=1e-2)
+        # self.critic_optim = Adam(self.critic.parameters(), lr=lr_c)
+        self.critic_optim = Adam(self.critic.parameters(), lr=lr_c, weight_decay=1e-3)
 
         # Network Initialization
         hard_update(self.actor_target, self.actor) 

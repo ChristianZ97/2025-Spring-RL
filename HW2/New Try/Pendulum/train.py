24d3d91f687fb742c298cb4d3590f841ddb4040d
Christian Jeng <kristan970724@gmail.com>
2025-04-26 21:41:50 +0800

    Tuning

--- HW2/New Try/Pendulum/train.py
+++ HW2/New Try/Pendulum/train.py
@@ -18,7 +18,8 @@ def agent_interact(writer, env, agent, memory, ounoise, total_numsteps, warm_up)
 
 		# Initialize state
         state_np, _ = env.reset()
-        episode_actions = [0]  # 記錄每個 episode 的動作
+        episode_actions = []
+
         # Interaction Loop
         while True:
 
@@ -32,7 +33,7 @@ def agent_interact(writer, env, agent, memory, ounoise, total_numsteps, warm_up)
                 with torch.no_grad():
                     action = agent.select_action(state_tensor, ounoise)
                 action_np = action.cpu().numpy()
-                # action_np = np.clip(action_np, env.action_space.low, env.action_space.high)
+                episode_actions.append(action_np)
 
             # Interact with the environment
             next_state_np, reward_np, terminated, truncated, _ = env.step(action_np)
@@ -45,10 +46,6 @@ def agent_interact(writer, env, agent, memory, ounoise, total_numsteps, warm_up)
             memory.push(state_np, action_np, mask_np, next_state_np, reward_np)
             state_np = next_state_np
 
-            # Break if episode end
-            #if done_np:
-            	#return total_numsteps
-
 
             # 每 1000 步記錄一次狀態和動作分佈
             if total_numsteps % 1000 == 0 and len(memory) > 0:
@@ -58,11 +55,13 @@ def agent_interact(writer, env, agent, memory, ounoise, total_numsteps, warm_up)
                 writer.add_histogram('Interact/State_Distribution', states, total_numsteps)
                 writer.add_histogram('Interact/Action_Distribution', actions, total_numsteps)
 
-            if done_np:
-                # 記錄每個 episode 的動作分佈
-                episode_actions = np.array(episode_actions, dtype=np.float32)
-                writer.add_histogram('Interact/Episode_Action_Distribution', episode_actions, total_numsteps)
-                return total_numsteps
+            # Break if episode end
+            if done_np: break
+
+        # 記錄每個 episode 的動作分佈
+        episode_actions = np.array(episode_actions, dtype=np.float32)
+        writer.add_histogram('Interact/Episode_Action_Distribution', episode_actions, total_numsteps)
+        return total_numsteps
 
 
 def agent_update(writer, agent, memory, batch_size, total_numsteps, updates_per_step, updates):
@@ -91,8 +90,8 @@ def agent_evaluate(writer, env, agent, i_episode, rewards, ewma_reward_history):
     state_np, _ = env.reset()
     t, episode_reward = 0, 0
     SOLVED = False
-
-    episode_actions = [] ##################
+    episode_actions = []
+    
     while True:
         state_tensor = torch.tensor(state_np, dtype=torch.float32)
         with torch.no_grad():
@@ -100,7 +99,7 @@ def agent_evaluate(writer, env, agent, i_episode, rewards, ewma_reward_history):
         action_np = action.cpu().numpy()
         # action_np = np.clip(action_np, env.action_space.low, env.action_space.high)
 
-        episode_actions.append(action_np) ############
+        episode_actions.append(action_np)
 
         next_state_np, reward_np, terminated, truncated, _ = env.step(action_np)
         done_np = terminated or truncated

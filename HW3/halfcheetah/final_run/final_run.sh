3ef0bb72b3f4169f1f497308b48482b64acd0263
Christian Jeng <kristan970724@gmail.com>
2025-05-23 17:13:01 +0800

    HW3

--- HW3/halfcheetah/final_run/final_run.sh
+++ HW3/halfcheetah/final_run/final_run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python sac_halfcheetah.py --lr=0.0009833543280434516 --discount-factor=0.98 --tau=0.008851313784063113
-python sac_halfcheetah.py --lr=0.0009332189626030728 --discount-factor=0.98 --tau=0.009325749191536416
-python sac_halfcheetah.py --lr=0.0006344571440300171 --discount-factor=0.98 --tau=0.002244255539895659
+python sac_halfcheetah.py --lr=0.0009833543280434516 --discount-factor=0.98 --tau=0.008851313784063113 --num-steps=1000000
+python sac_halfcheetah.py --lr=0.0009332189626030728 --discount-factor=0.98 --tau=0.009325749191536416 --num-steps=1000000
+python sac_halfcheetah.py --lr=0.0006344571440300171 --discount-factor=0.98 --tau=0.002244255539895659 --num-steps=1000000

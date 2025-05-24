d4f0782bc6970615125ec6bdc2e2a705e14b70fe
ChristianZ97 <124138449+ChristianZ97@users.noreply.github.com>
2025-05-24 09:36:28 +0800

    Delete HW3 directory

--- HW3/halfcheetah/final_run/final_run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python sac_halfcheetah.py --lr=0.0009833543280434516 --discount-factor=0.98 --tau=0.008851313784063113
-python sac_halfcheetah.py --lr=0.0009332189626030728 --discount-factor=0.98 --tau=0.009325749191536416
-python sac_halfcheetah.py --lr=0.0006344571440300171 --discount-factor=0.98 --tau=0.002244255539895659
